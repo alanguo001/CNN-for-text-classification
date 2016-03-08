c60b9a8f666e4b4fe0fa4c8f2e953e3c63b435cd
bwallace <byron.wallace@gmail.com>
2016-03-08 15:13:48 -0600

    refactoring

--- CNN_text.py
+++ CNN_text.py
@@ -12,6 +12,7 @@ Ye Zhang and Byron Wallace. "A Sensitivity Analysis of (and Practitioners' Guide
 '''
 
 from __future__ import print_function
+import pdb
 import sys
 reload(sys)
 sys.setdefaultencoding('utf8')
@@ -30,33 +31,13 @@ from keras.preprocessing.text import text_to_word_sequence, Tokenizer
 
 class TextCNN:
 
-    def __init__(self, max_features=10000, maxlen=300, embedding_dims=200, 
-                        filters=None, n_filters=100, dropout=0.0, wvs=None):
+    def __init__(self, preprocessor, filters=None, n_filters=100, dropout=0.0):
         '''
         parameters
         ---
-        max_features: the upper bound to be placed on the vocabulary size.
-        maxlen: the maximum length (in terms of tokens) of the instances/texts.
-        embedding_dims: size of the token embeddings; over-ridden if pre-trained
-                          vectors is provided (if wvs is not None).
-        filters: list of unique filter sizes (i.e., ngram sizes) to use.
-        n_filters: number of filters for each filter size.
-        dropout: dropout rate for penultimate layer.
-        wvs: pre-trained word vectors (e.g., trained Word2Vec model; assumes gensim object!)
+        preprocessor: an instance of the Preprocessor class, defined below
         '''
-        self.max_features = max_features  
-        self.tokenizer = Tokenizer(nb_words=self.max_features)
-        self.maxlen = maxlen  
-
-        self.use_pretrained_embeddings = False 
-        if wvs is None:
-            self.embedding_dims = embedding_dims
-        else:
-            # note that these are only for initialization;
-            # they will be tuned!
-            self.use_pretrained_embeddings = True
-            self.embedding_dims = wvs.vector_size
-            self.word_embeddings = wvs
+        self.preprocessor = preprocessor
 
         if filters is None:
             self.ngram_filters = [3, 4, 5]
@@ -65,46 +46,6 @@ class TextCNN:
 
         self.nb_filter = n_filters 
         self.dropout = dropout
-
-    def initialize_sequences_and_vocab(self, all_texts):
-        ''' 
-        This fits tokenizer and builds up input vectors (X) from the list 
-        of texts in all_texts. Needs to be called before train!
-        '''
-        self.raw_texts = all_texts
-        self.build_sequences()
-        if self.use_pretrained_embeddings:
-            self.init_word_vectors()
-
-    def build_sequences(self):
-        ''' Fits tokenizer and constructures sequences (i.e., X) '''
-        self.tokenizer.fit_on_texts(self.raw_texts)
-        self.word_indices_to_words = {}
-        for token, idx in self.tokenizer.word_index.items():
-            self.word_indices_to_words[idx] = token
-
-        self.X = list(self.tokenizer.texts_to_sequences_generator(self.raw_texts))
-        self.X = np.array(pad_sequences(self.X, maxlen=self.maxlen))
-
-    def init_word_vectors(self):
-        ''' 
-        Initialize word vectors.
-        '''
-        self.init_vectors = []
-        unknown_words_to_vecs = {}
-        for t, token_idx in self.tokenizer.word_index.items():
-            if token_idx <= self.max_features:
-                try:
-                    self.init_vectors.append(self.word_embeddings[t])
-                except:
-                    if t not in unknown_words_to_vecs:
-                        # randomly initialize
-                        unknown_words_to_vecs[t] = np.random.random(
-                                                self.embedding_dims)*-2 + 1
-
-                    self.init_vectors.append(unknown_words_to_vecs[t])
-
-        self.init_vectors = np.vstack(self.init_vectors)
 
     
     def train(self, X_train, y_train, X_val=None, y_val=None,
@@ -140,13 +81,11 @@ class TextCNN:
     def build_model(self):
         # again, credit to Cheng Guo
         self.model = Graph()
-        self.model.add_input(name='input', input_shape=(self.maxlen,), dtype=int)
-        init_embeddings = None 
-        if self.use_pretrained_embeddings:
-            init_embeddings = [self.init_vectors]
+        self.model.add_input(name='input', input_shape=(self.preprocessor.maxlen,), dtype=int)
 
-        self.model.add_node(Embedding(self.max_features, self.embedding_dims, 
-                                input_length=self.maxlen, weights=init_embeddings), 
+        #pdb.set_trace()
+        self.model.add_node(Embedding(self.preprocessor.max_features, self.preprocessor.embedding_dims, 
+                                input_length=self.preprocessor.maxlen, weights=self.preprocessor.init_vectors), 
                                 name='embedding', input='input')
         self.model.add_node(Dropout(0.), name='dropout_embedding', input='embedding')
         for n_gram in self.ngram_filters:
@@ -155,11 +94,11 @@ class TextCNN:
                                          border_mode='valid',
                                          activation='relu',
                                          subsample_length=1,
-                                         input_dim=self.embedding_dims,
-                                         input_length=self.maxlen),
+                                         input_dim=self.preprocessor.embedding_dims,
+                                         input_length=self.preprocessor.maxlen),
                            name='conv_' + str(n_gram),
                            input='dropout_embedding')
-            self.model.add_node(MaxPooling1D(pool_length=self.maxlen - n_gram + 1),
+            self.model.add_node(MaxPooling1D(pool_length=self.preprocessor.maxlen - n_gram + 1),
                            name='maxpool_' + str(n_gram),
                            input='conv_' + str(n_gram))
             self.model.add_node(Flatten(),
@@ -172,4 +111,75 @@ class TextCNN:
         self.model.add_output(name='output', input='sigmoid')
         print("model built")
         print(self.model.summary())
+
+
+class Preprocessor:
+    def __init__(self, max_features, maxlen, embedding_dims=200, wvs=None):
+        '''
+        max_features: the upper bound to be placed on the vocabulary size.
+        maxlen: the maximum length (in terms of tokens) of the instances/texts.
+        embedding_dims: size of the token embeddings; over-ridden if pre-trained
+                          vectors is provided (if wvs is not None).
+        '''
+
+        self.max_features = max_features  
+        self.tokenizer = Tokenizer(nb_words=self.max_features)
+        self.maxlen = maxlen  
+
+        self.use_pretrained_embeddings = False 
+        self.init_vectors = None 
+        if wvs is None:
+            self.embedding_dims = embedding_dims
+        else:
+            # note that these are only for initialization;
+            # they will be tuned!
+            self.use_pretrained_embeddings = True
+            self.embedding_dims = wvs.vector_size
+            self.word_embeddings = wvs
+
+
+    def preprocess(self, all_texts):
+        ''' 
+        This fits tokenizer and builds up input vectors (X) from the list 
+        of texts in all_texts. Needs to be called before train!
+        '''
+        self.raw_texts = all_texts
+        #self.build_sequences()
+        self.fit_tokenizer()
+        if self.use_pretrained_embeddings:
+            self.init_word_vectors()
+
+    def fit_tokenizer(self):
+        ''' Fits tokenizer to all raw texts; remembers indices->words mappings. '''
+        self.tokenizer.fit_on_texts(self.raw_texts)
+        self.word_indices_to_words = {}
+        for token, idx in self.tokenizer.word_index.items():
+            self.word_indices_to_words[idx] = token
+
+    def build_sequences(self, texts):
+        X = list(self.tokenizer.texts_to_sequences_generator(texts))
+        X = np.array(pad_sequences(X, maxlen=self.maxlen))
+        return X
+
+    def init_word_vectors(self):
+        ''' 
+        Initialize word vectors.
+        '''
+        self.init_vectors = []
+        unknown_words_to_vecs = {}
+        for t, token_idx in self.tokenizer.word_index.items():
+            if token_idx <= self.max_features:
+                try:
+                    self.init_vectors.append(self.word_embeddings[t])
+                except:
+                    if t not in unknown_words_to_vecs:
+                        # randomly initialize
+                        unknown_words_to_vecs[t] = np.random.random(
+                                                self.embedding_dims)*-2 + 1
+
+                    self.init_vectors.append(unknown_words_to_vecs[t])
+
+        # note that we make this a singleton list because that's
+        # what Keras wants. 
+        self.init_vectors = [np.vstack(self.init_vectors)]
 
