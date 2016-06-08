54455cf51a0d004e60b946ca7742f1e414b8b407
bwallace <byron.wallace@gmail.com>
2016-06-08 16:42:34 -0500

    terrible failure

--- rationale_CNN.py
+++ rationale_CNN.py
@@ -1,8 +1,9 @@
 '''
 @author Byron Wallace
-A Keras implementation of our "rationale augmented CNN" (https://arxiv.org/abs/1605.04469).
+A Keras implementation of our "rationale augmented CNN" (https://arxiv.org/abs/1605.04469). Please note that
+the model was originally implemented in Theano -- this version is a work in progress.
 
-Credit for initial pass of implementation to: Cheng Guo (https://gist.github.com/entron).
+Credit for initial pass of basic CNN implementation to: Cheng Guo (https://gist.github.com/entron).
 
 References
 --
@@ -21,15 +22,13 @@ sys.setdefaultencoding('utf8')
 
 import numpy as np
 
-#import nltk # for sentence splitting 
-#from nltk.tokenize import sent_tokenize
-
 from keras.preprocessing import sequence
+from keras.engine.topology import Layer
 from keras.preprocessing.sequence import pad_sequences
 from keras.models import Graph
 from keras.layers.core import Dense, Dropout, Activation, Flatten, Merge
 from keras.layers.embeddings import Embedding
-from keras.layers.convolutional import Convolution1D, MaxPooling1D
+from keras.layers.convolutional import Convolution1D, Convolution2D, MaxPooling1D
 from keras.datasets import imdb
 from keras.utils.np_utils import accuracy
 from keras.preprocessing.text import text_to_word_sequence, Tokenizer
@@ -39,6 +38,64 @@ from keras.callbacks import ModelCheckpoint
 from keras.layers import Input, Embedding, Dense, merge
 from keras.models import Model
 
+from keras import backend as K 
+
+class SentenceConvolutionLayer(Layer):
+    # input shape: (sentences, max_tokens)
+    
+    def __init__(self):
+        pass 
+
+class WeightedSumSentenceVector2(Layer):
+    # input shape: (sentences, sentence_embedding_dim)
+    #       # documents[0].sentence_sequences
+    # output shape: (1, sentence_embedding_dim)
+
+    def __init__(self, sentence_model, **kwargs):
+        #self.sentence_cnn = sentence_cnn
+        #self.sentence_model = sentence_model
+         pass 
+
+class WeightedSumSentenceVector(Layer):
+    # input shape: (sentences, sentence_embedding_dim)
+    #       # documents[0].sentence_sequences
+    # output shape: (1, sentence_embedding_dim)
+
+    def __init__(self, sentence_model, **kwargs):
+        #self.sentence_cnn = sentence_cnn
+        self.sentence_model = sentence_model
+
+
+        super(WeightedSumSentenceVector, self).__init__(**kwargs)
+
+    def get_output_shape_for(self, input_shape):
+        assert len(shape) == 2
+        return (1, input_shape[1])
+
+    def call(self, X, mask=None):
+        '''
+        X assumed to be a document, with each row a list of 
+        token indicators in the corresponding sentences. 
+        '''
+        conv_f = K.function(
+                        [self.sentence_model.layers[0].input, K.learning_phase()], 
+                        [self.sentence_model.layers[-2].output])
+
+        import pdb; pdb.set_trace()
+        sentence_vectors = conv_f([X,1])[0]
+
+        sentence_predictions = sentence_model.predict([test_sentences])
+        weights = np.amax(sentence_predictions[:,0:2],axis=1)
+
+        return np.matrix(np.dot(weights, vecs))
+
+    def build(self, input_shape):
+
+        #input_dim = input_shape[1]
+        #initial_weight_value = np.random.random((input_dim, output_dim))
+        #self.W = K.variable(initial_weight_value)
+        #self.trainable_weights = [self.W]
+        self.trainable_weights = self.sentence_model.trainable_weights
 
 class RationaleCNN:
 
@@ -57,6 +114,7 @@ class RationaleCNN:
 
         self.nb_filter = n_filters 
         self.dropout = dropout
+        self.sentence_model_trained = False 
 
         #self.build_model() # build model
         #self.train_sentence_model()
@@ -77,7 +135,7 @@ class RationaleCNN:
         return X[train_indices,:], y[train_indices]
 
     # r_CNN.sentence_model.predict(X[:10], batch_size=128)
-    def train_sentence_model(self, train_documents, nb_epoch=5, batch_size=128, optimizer='adam'):
+    def train_sentence_model(self, train_documents, nb_epoch=5, downsample=True, batch_size=128, optimizer='adam'):
         # assumes sentence sequences have been generated!
         assert(train_documents[0].sentence_sequences is not None)
 
@@ -91,10 +149,13 @@ class RationaleCNN:
         X, y = np.asarray(X), np.asarray(y)
         
         # downsample
-        X_ds, y_ds = RationaleCNN.balanced_sample(X, y)
+        if downsample:
+            X, y = RationaleCNN.balanced_sample(X, y)
 
         #self.train(X[:1000], y[:1000])
-        self.train(X_ds, y_ds)
+        self.train(X, y)
+
+        self.sentence_model_trained = True
 
 
     def train(self, X_train, y_train, X_val=None, y_val=None,
@@ -105,8 +166,6 @@ class RationaleCNN:
 
         X_val and y_val are to be used to validate during training. 
         '''
-
-
         checkpointer = ModelCheckpoint(filepath="weights.hdf5", 
                                        verbose=1, 
                                        save_best_only=(X_val is not None))
@@ -139,9 +198,13 @@ class RationaleCNN:
 
 
     def build_sentence_model(self):
-        tokens_input = Input(name='input', shape=(self.preprocessor.maxlen,), dtype='int32')
+        ''' 
+        Build the *sentence* level model, which operates over, erm, sentences. 
+        The task is to predict which sentences are pos/neg rationales.
+        '''
+        tokens_input = Input(name='input', shape=(self.preprocessor.max_sent_len,), dtype='int32')
         x = Embedding(self.preprocessor.max_features, self.preprocessor.embedding_dims, 
-                      input_length=self.preprocessor.maxlen, 
+                      input_length=self.preprocessor.max_sent_len, 
                       weights=self.preprocessor.init_vectors)(tokens_input)
         
         x = Dropout(0.1)(x)
@@ -154,14 +217,14 @@ class RationaleCNN:
                                          activation='relu',
                                          subsample_length=1,
                                          input_dim=self.preprocessor.embedding_dims,
-                                         input_length=self.preprocessor.maxlen)(x)
+                                         input_length=self.preprocessor.max_sent_len)(x)
             # pool
-            one_max = MaxPooling1D(pool_length=self.preprocessor.maxlen - n_gram + 1)(cur_conv)
+            one_max = MaxPooling1D(pool_length=self.preprocessor.max_sent_len - n_gram + 1)(cur_conv)
             flattened = Flatten()(one_max)
             convolutions.append(flattened)
 
-        penultimate_layer = merge(convolutions)
-        output = Dense(3, activation="softmax")(penultimate_layer)
+        sentence_vector = merge(convolutions, name="sentence_vector") # hang on to this layer!
+        output = Dense(3, activation="softmax")(sentence_vector)
 
         self.sentence_model = Model(input=tokens_input, output=output)
         print("model built")
@@ -171,8 +234,159 @@ class RationaleCNN:
         return self.sentence_model 
 
 
+    def weighted_sum(x, probs):
+        pass 
+
+
+    def build_doc_model_fixed(self, train_documents):
+        conv_f = K.function(
+                        [self.sentence_model.layers[0].input, K.learning_phase()], 
+                        [self.sentence_model.layers[-2].output])
+
+        
+        
+
+        sentence_vectors = conv_f([X,1])[0]
+
+        sentence_predictions = sentence_model.predict([test_sentences])
+        weights = np.amax(sentence_predictions[:,0:2],axis=1)
+
+        return np.matrix(np.dot(weights, vecs))
+
+    def build_doc_model3(self):
+        model = Sequential()
+
+        document_input = Input(name='input', 
+            shape=(None, self.preprocessor.max_doc_len, 
+                   self.preprocessor.max_sent_len), dtype='int32')
+
+        # filter, nb_rows, nb_cols
+        n_gram = 1
+        cur_conv = Convolution2D(32, 
+                             n_gram, self.preprocessor.embedding_dims, 
+
+                             activation='relu',
+                             # samples, channels, rows, cols
+                             input_shape=(1,
+                                self.preprocessor.max_doc_len,
+                                self.preprocessor.embedding_dims,
+                             )(document_input)
+
+    def build_doc_model2(self):
+        document_input = Input(name='input', 
+            shape=(self.preprocessor.max_doc_len, 
+                   self.preprocessor.max_sent_len,), dtype='int32')
+
+        document_vector = WeightedSumSentenceVector(self.sentence_model)(document_input)
+
+        # sentence_vectors = 
+
+        # 
+        #conv_f = K.function([self.sentence_model.layers[0].input, K.learning_phase()], 
+        #                [self.sentence_model.layers[-2].output])
+        # test_sent.shape
+        #   (1,50) ### this is the list of token indices!
+        # sentence_v = conv_f([test_sent,1])[0]
+
+        ''' 
+        Re-construct the (start of) the *sentence* level model, which operates over, erm, sentences. 
+        The task is to predict which sentences are pos/neg rationales.
+        '''
+        # 
+
+
+
+        '''
+        tokens_input = Input(name='input', shape=(self.preprocessor.max_sent_len,), dtype='int32')
+        x = Embedding(self.preprocessor.max_features, self.preprocessor.embedding_dims, 
+                      input_length=self.preprocessor.max_sent_len, 
+                      weights=self.preprocessor.init_vectors)(tokens_input)
+        
+        x = Dropout(0.1)(x)
+
+        convolutions = []
+        for n_gram in self.ngram_filters:
+            cur_conv = Convolution1D(nb_filter=self.nb_filter,
+                                         filter_length=n_gram,
+                                         border_mode='valid',
+                                         activation='relu',
+                                         subsample_length=1,
+                                         input_dim=self.preprocessor.embedding_dims,
+                                         input_length=self.preprocessor.max_sent_len)(x)
+            # pool
+            one_max = MaxPooling1D(pool_length=self.preprocessor.max_sent_len - n_gram + 1)(cur_conv)
+            flattened = Flatten()(one_max)
+            convolutions.append(flattened)
+
+        sentence_vector = merge(convolutions, name="sentence_vector") # hang on to this layer!
+        '''
+
+        # ok initialize each layer with parameters!
+
+
+        ###
+        # 
+        '''
+        output = Dense(3, activation="softmax")(self.penultimate_layer)
+
+        self.sentence_model = Model(input=tokens_input, output=output)
+        '''
+
     def build_doc_model(self):
-        # again, credit to Cheng Guo
+        '''
+        Builds the *document* level model, which uses the sentence level model to inform
+        its predictions.
+        '''
+        tokens_input = Input(name='input', shape=(p.max_doc_len, p.max_sent_len,), dtype='int32')
+        
+        x = Embedding(self.preprocessor.max_features, self.preprocessor.embedding_dims, 
+                      input_length=self.preprocessor.max_sent_len, 
+                      weights=self.preprocessor.init_vectors)(tokens_input)
+        
+        x = Dropout(0.1)(x)
+
+        convolutions = []
+        for n_gram in self.ngram_filters:
+            #cur_conv = Convolution1D(nb_filter=self.nb_filter, filter_length=n_gram)
+
+            '''
+            # filter, nb_rows, nb_cols
+            cur_conv = Convolution2D(self.nb_filter, 
+                             1, self.preprocessor.embedding_dims, 
+                             filter_length=n_gram,
+                             activation='relu',
+                             input_dim=self.preprocessor.embedding_dims,
+                             input_length=self.preprocessor.max_sent_len)(x)
+            '''
+
+            # cur_conv = Convolution2D(32, p.embedding_dims, n_gram, input_shape=(1, p.embedding_dims,  p.max_sent_len))(x)
+            cur_conv = Convolution1D(nb_filter=self.nb_filter,
+                                         filter_length=n_gram,
+                                         border_mode='valid',
+                                         activation='relu',
+                                         subsample_length=1,
+                                         input_dim=self.preprocessor.embedding_dims,
+                                         input_length=self.preprocessor.max_sent_len)(x)
+            
+
+            # pool
+            one_max = MaxPooling1D(pool_length=self.preprocessor.max_sent_len - n_gram + 1)(cur_conv)
+            flattened = Flatten()(one_max)
+            convolutions.append(flattened)
+
+        penultimate_layer = merge(convolutions)
+        
+        output = Dense(1, activation="sigmoid")(penultimate_layer)
+
+        self.document_model = Model(input=tokens_input, output=output)
+        
+        print(self.document_model.summary())
+        self.document_model.compile(loss='binary_crossentropy', optimizer="adam")
+
+        return self.document_model 
+
+
+        '''
         self.model = Graph()
         self.model.add_input(name='input', input_shape=(self.preprocessor.maxlen,), dtype=int)
 
@@ -205,7 +419,7 @@ class RationaleCNN:
         print(self.model.summary())
         self.model.compile(loss={'output': 'binary_crossentropy'}, 
                                 optimizer="adam")#optimizer)
-
+        '''
 
 class Document:
     def __init__(self, doc_id, sentences, doc_label=None, sentences_labels=None):
@@ -251,17 +465,18 @@ class Document:
 
 
 class Preprocessor:
-    def __init__(self, max_features, maxlen, embedding_dims=200, wvs=None):
+    def __init__(self, max_features, max_sent_len, embedding_dims=200, wvs=None, max_doc_len=500):
         '''
         max_features: the upper bound to be placed on the vocabulary size.
-        maxlen: the maximum length (in terms of tokens) of the instances/texts.
+        max_sent_len: the maximum length (in terms of tokens) of the instances/texts.
         embedding_dims: size of the token embeddings; over-ridden if pre-trained
                           vectors is provided (if wvs is not None).
         '''
 
         self.max_features = max_features  
         self.tokenizer = Tokenizer(nb_words=self.max_features)
-        self.maxlen = maxlen  
+        self.max_sent_len = max_sent_len  # the max sentence length! @TODO rename; this is confusing. 
+        self.max_doc_len = max_doc_len # w.r.t. number of sentences!
 
         self.use_pretrained_embeddings = False 
         self.init_vectors = None 
@@ -296,7 +511,7 @@ class Preprocessor:
 
     def build_sequences(self, texts):
         X = list(self.tokenizer.texts_to_sequences_generator(texts))
-        X = np.array(pad_sequences(X, maxlen=self.maxlen))
+        X = np.array(pad_sequences(X, maxlen=self.max_sent_len))
         return X
 
     def init_word_vectors(self):
